10c6bd049c1bda1320c30a2e2e285d78a4957cc7
Jayden Windle <jaydenwindle@gmail.com>
2025-10-23 14:42:31 -0700

    Added API authorization config

--- ipfs/init.sh
+++ ipfs/init.sh
@@ -7,10 +7,23 @@ ipfs config --json Gateway.NoFetch true
 # Disable DNSLink resolution globally
 ipfs config --json Gateway.NoDNSLink true
 
-# Enable DNSLink for yourdomain.com
+# Enable DNSLink for jaydenwindle.com
 ipfs config --json Gateway.PublicGateways '{"jaydenwindle.com": {"NoDNSLink": false, "Paths": []}}'
 
 # Set identity PrivKey from environment variable if provided
 if [ -n "$IPFS_IDENTITY_PRIVKEY" ]; then
   ipfs config Identity.PrivKey "$IPFS_IDENTITY_PRIVKEY"
+fi
+
+# Configure deployer auth for API endpoints if provided
+if [ -n "$IPFS_API_AUTH_TOKEN" ]; then
+  ipfs config --json API.Authorizations '{
+    "deployer": {
+      "AuthSecret": "bearer:'"$IPFS_API_AUTH_TOKEN"'",
+      "AllowedPaths": [
+        "/api/v0/name/publish",
+        "/api/v0/dag/import"
+      ]
+    }
+  }'
 fi
